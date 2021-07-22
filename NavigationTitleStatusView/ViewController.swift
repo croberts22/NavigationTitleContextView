1eb1304777615215337d2039c8987961c666dcca
Corey Roberts <spacepyro@protonmail.com>
2021-07-22 13:10:16 -0500

    Initial Commit

---
+++ NavigationTitleStatusView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationTitleStatusView
+//
+//  Created by Corey Roberts on 7/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
